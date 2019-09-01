47fc5c55aa0dcc7c531146fc55775d7136eca2b4
FalaahAzmi <54742143+falahazmi@users.noreply.github.com>
2019-09-02 02:15:45 +0700

    Add files via upload

---
+++ src/1_Pengantar_Konsep_PBO/SepedaGunung1841720085Falaah.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SepedaGunung1841720085Falaah extends Sepeda1841720085Falaah
+{
+    private String tipeSuspensi;
+    
+    public void setTipeSuspensi(String newValue) 
+    {
+        tipeSuspensi = newValue;
+    }
+    
+    public void cetakSatus() 
+    {
+        super.cetakSatus();
+        System.out.println("Tipe Suspensi: "+tipeSuspensi);
+    }
+}
+
